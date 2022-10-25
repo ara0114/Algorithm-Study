3fd04cec38ca6d0ea60939b4df48a7a8857b9fcc
arajo0114 <arajo0114a@gmail.com>
2022-10-25 22:00:00 +0900

    Level2 피보나치 수

---
+++ 프로그래머스/피보나치 수.java
@@ -0,0 +1,19 @@
+class Solution {    
+    public int solution(int n) {
+        int answer = 0;        
+        int[] arr = new int[n+1];
+        
+        for(int i = 0; i <= n; i++){
+            if(i == 0 || i == 1){
+                arr[i] = i;
+            }else{
+                arr[i] = arr[i-1] % 1234567 + arr[i-2] % 1234567;
+            }
+        }
+        
+        answer = arr[n] % 1234567;
+        
+        return answer;
+    }
+    
+}
